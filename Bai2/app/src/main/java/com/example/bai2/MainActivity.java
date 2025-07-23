d5627a37d69e63eaef40fb3e74c66f636dc2dc29
thanvu1 <hoangthanvu11@gmail.com>
2025-07-23 07:48:33 +0700

    them chuc nang

--- Bai2/app/src/main/java/com/example/bai2/MainActivity.java
+++ Bai2/app/src/main/java/com/example/bai2/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.bai2;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,24 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.inappmessaging.model.Button;
+
+public class MainActivity extends Activity {
+    Button btncall;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        Toast.makeText(this, "CR424 - onCreat()", Toast.LENGTH_SHORT).show();
+        btncall = findViewById(R.id.btncall);
+        btncall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent intent1 = new Intent(MainActivity.this, subactivity.class);
+                startActivity(intent1);
+            }
         });
     }
 }
