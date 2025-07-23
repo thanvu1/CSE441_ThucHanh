9b0f2033f3106fee37a5473046a305bc241c24ef
thanvu1 <hoangthanvu11@gmail.com>
2025-07-23 08:30:26 +0700

    chinh sua

--- Bai2/app/src/main/java/com/example/bai2/MainActivity.java
+++ Bai2/app/src/main/java/com/example/bai2/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.bai2;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -10,10 +13,46 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.google.firebase.inappmessaging.model.Button;
 
 public class MainActivity extends Activity {
     Button btncall;
+
+    @Override
+    protected void onDestroy() {
+// TODO Auto-generated method stub
+        super.onDestroy();
+        Toast.makeText(this,"CR424 - on Destroy()",Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onPause() {
+// TODO Auto-generated method stub
+        super.onPause();
+        Toast.makeText(this," CR424 - onPause", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onRestart() {
+// TODO Auto-generated method stub
+        super.onRestart();
+        Toast.makeText(this," CR424 - onRestart", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onResume() {
+// TODO Auto-generated method stub
+        super.onResume();
+        Toast.makeText(this," CR424 - onResume", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onStart() {
+// TODO Auto-generated method stub
+        super.onStart();
+        Toast.makeText(this," CR424 - onStart", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onStop() {
+// TODO Auto-generated method stub
+        super.onStop();
+        Toast.makeText(this," CR424 - onStop", Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +64,7 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View v) {
                 // TODO Auto-generated method stub
-                Intent intent1 = new Intent(MainActivity.this, subactivity.class);
+                Intent intent1 = new Intent(MainActivity.this, Subactivity.class);
                 startActivity(intent1);
             }
         });
