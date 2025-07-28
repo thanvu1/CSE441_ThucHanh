a786a444ccc526e34e79c8ce8ff0ac6b04683537
thanvu1 <hoangthanvu11@gmail.com>
2025-07-28 21:43:32 +0700

    add function

--- Bai4/Bai4_1/app/src/main/java/com/example/bai41/MainActivity.kt
+++ Bai4/Bai4_1/app/src/main/java/com/example/bai41/MainActivity.kt
@@ -1,20 +1,44 @@
 package com.example.bai41
 
+import android.app.Activity
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+import android.widget.EditText
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
+    private lateinit var edtdoC: EditText
+    private lateinit var edtdoF: EditText
+    private lateinit var btncf: Button
+    private lateinit var btnfc: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        edtdoC = findViewById(R.id.txtCel)
+        edtdoF = findViewById(R.id.txtFar)
+        btncf = findViewById(R.id.btnCel)
+        btnfc = findViewById(R.id.btnFar)
+        val btnClear = findViewById<Button>(R.id.btnClear)
+
+        btncf.setOnClickListener {
+            val doC = edtdoC.text.toString()
+            if (doC.isNotEmpty()) {
+                val C = doC.toDouble()
+                edtdoF.setText(String.format("%.2f", C * 1.8 + 32))
+            }
+        }
+
+        btnfc.setOnClickListener {
+            val doF = edtdoF.text.toString()
+            if (doF.isNotEmpty()) {
+                val F = doF.toDouble()
+                edtdoC.setText(String.format("%.2f", (F - 32) / 1.8))
+            }
+        }
+
+        btnClear.setOnClickListener {
+            edtdoC.text.clear()
+            edtdoF.text.clear()
         }
     }
 }
