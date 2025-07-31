ce77fe53037605dd678eca53441c6e68d417aae1
thanvu1 <hoangthanvu11@gmail.com>
2025-07-31 21:48:26 +0700

    fix ui

--- Bai6/app/build.gradle.kts
+++ Bai6/app/build.gradle.kts
@@ -36,7 +36,7 @@ android {
 }
 
 dependencies {
-
+    implementation("com.squareup.picasso:picasso:2.8")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
