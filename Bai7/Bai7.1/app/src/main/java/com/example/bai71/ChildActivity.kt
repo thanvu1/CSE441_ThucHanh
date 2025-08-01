8be221c242fb7ebefa2172171d2083878c6c79b0
thanvu1 <hoangthanvu11@gmail.com>
2025-08-01 09:19:25 +0700

    update ui bai7.1

--- Bai7/Bai7.1/app/src/main/java/com/example/bai71/ChildActivity.kt
+++ Bai7/Bai7.1/app/src/main/java/com/example/bai71/ChildActivity.kt
@@ -4,17 +4,19 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 
-class MainActivity : AppCompatActivity() {
+class child_activity : AppCompatActivity() {
     protected override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val btn1 = findViewById(R.id.button1) as Button
-        btn1.setOnClickListener(object : View.OnClickListener {
+        setContentView(R.layout.child_activity)
+        val btn = findViewById(R.id.button1) as Button
+        val txt1 = findViewById(R.id.textView1) as TextView?
+        btn.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
-                val intent1: Intent = Intent(this@MainActivity, ChildActivity::class.java)
+                val intent1: Intent = Intent(this@child_activity, MainActivity::class.java)
                 startActivity(intent1)
             }
         })
