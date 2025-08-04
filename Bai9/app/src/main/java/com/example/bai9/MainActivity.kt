a34c4237ce268d0aabe2762cc355aa790b813ded
thanvu1 <hoangthanvu11@gmail.com>
2025-08-04 10:31:59 +0700

    feature(music): add play and stop musc funcion

--- Bai9/app/src/main/java/com/example/bai9/MainActivity.kt
+++ Bai9/app/src/main/java/com/example/bai9/MainActivity.kt
@@ -1,20 +1,49 @@
 package com.example.bai9
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.ImageButton
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.squareup.picasso.Picasso
+
 
 class MainActivity : AppCompatActivity() {
+    var btnplay: ImageButton? = null
+    var btnstop: ImageButton? = null
+    var flag: Boolean = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        btnplay = findViewById<ImageButton?>(R.id.imagstart)
+        btnstop = findViewById<ImageButton?>(R.id.imgstop)
+        val imageView1 = findViewById<ImageView>(R.id.imageView1)
+        Picasso.get().load("https://static.vecteezy.com/system/resources/thumbnails/031/617/767/small_2x/abstract-music-background-with-notes-and-bokeh-lights-illustration-music-icon-song-time-wavy-shape-generative-ai-photo.jpg").into(imageView1)
+        Picasso.get().load("https://cdn-icons-png.flaticon.com/512/3318/3318660.png").into(btnplay) // icon play
+        Picasso.get().load("https://cdn-icons-png.flaticon.com/512/727/727243.png").into(btnstop)
+
+        // Xử lý click
+        btnplay!!.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View?) {
+                val intent1 = Intent(this@MainActivity, MyService::class.java)
+                startService(intent1)
+                if (flag == true) {
+                    Picasso.get().load("https://www.svgrepo.com/show/390471/media-music-pause-play-song-stop.svg").into(btnplay)
+                    flag = false
+                } else {
+                    Picasso.get().load("https://svgsilh.com/svg/468294.svg").into(btnplay)
+                    flag = true
+                }
+            }
+        })
+        btnstop!!.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View?) {
+                val intent2 = Intent(this@MainActivity, MyService::class.java)
+                stopService(intent2)
+                Picasso.get().load("https://cdn-icons-png.flaticon.com/512/3318/3318660.png").into(btnplay)
+                flag = true
+            }
+        })
     }
 }
--- Bai9/app/src/main/java/com/example/bai9/MyService.kt
+++ Bai9/app/src/main/java/com/example/bai9/MyService.kt
@@ -2,11 +2,38 @@ package com.example.bai9
 
 import android.app.Service
 import android.content.Intent
+import android.media.MediaPlayer
 import android.os.IBinder
 
 class MyService : Service() {
+    private var mymedia: MediaPlayer? = null
 
-    override fun onBind(intent: Intent): IBinder {
-        TODO("Return the communication channel to the service.")
+    override fun onBind(intent: Intent?): IBinder? {
+        // Not used in this example
+        throw UnsupportedOperationException("Not yet implemented")
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        mymedia = MediaPlayer.create(this, R.raw.sally_shigeaki_saegusa)
+        mymedia?.isLooping = true
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        mymedia?.let {
+            if (it.isPlaying) {
+                it.pause()
+            } else {
+                it.start()
+            }
+        }
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mymedia?.stop()
+        mymedia?.release()
+        mymedia = null
     }
 }
