9045ad664d684eded97ee309252c8a53031af037
thanvu1 <hoangthanvu11@gmail.com>
2025-08-07 10:15:01 +0700

    feat: add spinner functionality to main activity and update layout

--- Bai12/Bai12.3/app/src/main/java/com/example/spinner/MainActivity.kt
+++ Bai12/Bai12.3/app/src/main/java/com/example/spinner/MainActivity.kt
@@ -1,20 +1,46 @@
 package com.example.spinner
 
+import android.app.Activity
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.view.View
+import android.widget.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
+    private val arr1 = arrayOf(
+        "Hàng Điện tử", "Hàng Hóa Chất",
+        "Hàng Gia dụng", "Hàng xây dựng"
+    )
+
+    private lateinit var txt1: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        txt1 = findViewById(R.id.txt1)
+        val spin1 = findViewById<Spinner>(R.id.spinner1)
+
+        val adapter1 = ArrayAdapter(
+            this,
+            android.R.layout.simple_spinner_item,
+            arr1
+        )
+        adapter1.setDropDownViewResource(android.R.layout.simple_list_item_single_choice)
+        spin1.adapter = adapter1
+
+        spin1.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                txt1.text = arr1[position]
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // Không làm gì cả
+            }
         }
     }
 }
