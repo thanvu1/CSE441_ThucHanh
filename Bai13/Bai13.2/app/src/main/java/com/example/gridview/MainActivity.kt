490cf4280d047f884ff7f11cb83fef1d53a02e6d
thanvu1 <hoangthanvu11@gmail.com>
2025-08-07 15:51:52 +0700

    feat: add gridview layout and display the selection

--- Bai13/Bai13.2/app/src/main/java/com/example/gridview/MainActivity.kt
+++ Bai13/Bai13.2/app/src/main/java/com/example/gridview/MainActivity.kt
@@ -1,20 +1,39 @@
 package com.example.gridview
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.GridView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private val arr = arrayOf(
+        "Ipad", "Iphone", "New Ipad",
+        "SamSung", "Nokia", "Sony Ericson",
+        "LG", "Q-Mobile", "HTC", "Blackberry",
+        "G Phone", "FPT - Phone", "HK Phone"
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val selection = findViewById<TextView>(R.id.selection)
+        val gv = findViewById<GridView>(R.id.gridView1)
+
+        val adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_1,
+            arr
+        )
+        gv.adapter = adapter
+
+        gv.setOnItemClickListener { _, _, position, _ ->
+            selection.text = arr[position]
         }
     }
 }
